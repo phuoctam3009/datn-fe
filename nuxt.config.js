f325021834960bda4b2b387fda0cda7f9f59e0b5
phuoctam30091999 <tamnp@irtech.com.vn>
2021-11-22 09:35:41 +0700

    Init

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+// nuxt.config.js
+{
+  buildModules: [
+    // Simple usage
+    "@nuxtjs/vuetify",
+
+    // With options
+    [
+      "@nuxtjs/vuetify",
+      {
+        /* module options */
+      },
+    ],
+  ];
+}
---
+++ src/main.js
@@ -0,0 +1,14 @@
+// Polyfills
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
